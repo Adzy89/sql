5df4312d77b7d1c4039c48c5c8346814688aa5b4
adzzy89 <adam_pilato@hotmail.com>
2023-02-10 22:31:54 +1100

    added role table and empolyee table

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,6 @@
--- Deletes the database if exists
+
 DROP DATABASE IF EXISTS empolyees_db;
 
--- creates new db
 CREATE DATABASE empolyees_db;
 
 USE empolyees_db;
@@ -9,12 +8,28 @@ USE empolyees_db;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE role (
-    id INT NOT NULL 
-    tittle VARCHAR(30)
-    salary DECIMAL
-    department
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tittle VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+    manager_id INT, 
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 )
